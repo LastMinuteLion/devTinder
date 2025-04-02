17ef27ef5cfaeeda02becb30bf415b69a5d50341
Jellyyyyy05 <121446100+Jellyyyyy05@users.noreply.github.com>
2025-04-03 00:32:21 +0530

    exploring

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get("/test",(req,res) => {
+    res.send("Helo from Server")
+})
+
 app.use("/test",(req,res) => {
     res.send("Helo from Server")
 })
