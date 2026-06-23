c6572d546dfd922a63370494cf6cd0c06ec90e2b
Harsh Vardhan <harsh20033@gmail.com>
2026-06-23 20:09:36 +0530

    Replaced URI

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://HarshV05:GM37cRzklYxeK9ud@cluster0.iweyvpj.mongodb.net/devTinder");
+    await mongoose.connect(process.env.MONGO_URI);
 };
 
 
